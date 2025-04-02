543f1430ff894a0896ca985adbc7602764775ca9
GiovanniP9 <pisaniellogiovanni53@gmail.com>
2025-04-02 15:10:52 +0200

    Create decoratori.py

---
+++ Mercoledi 2 Aprile/decoratori.py
@@ -0,0 +1,44 @@
+
+def decoratore(funzione):
+ def wrapper():
+   print("Prima dell'esecuzione della funzione")
+   funzione()
+   print("Dopo l'esecuzione della funzione")
+ return wrapper
+
+@decoratore
+def saluta():
+    print("Ciao, benvenuto!")
+
+saluta() 
+
+def decoratore_con_argomenti(funzione):
+ def wrapper(*args, **kwargs):
+  print("Prima")
+  risultato = funzione(*args, **kwargs)
+  print("Dopo")
+  return risultato + 2
+ return wrapper
+
+@decoratore_con_argomenti
+def somma(a, b):
+    print(a+b)
+    return a + b
+print(somma(3, 4))
+
+def logger(funzione):
+    def wrapper(*args, **kwargs):
+     print(f"Chiamata a {funzione.__name__} con argomenti: {args} e {kwargs}")# A E B non escono in kwargs perche non sono keyword
+     risultato = funzione(*args, **kwargs)
+     print(f"Risultato di {funzione.__name__}: {risultato}")
+     return risultato
+    return wrapper
+
+@logger
+def moltiplica(a, b):
+    return a * b
+
+# Chiamata alla funzione decorata
+print(moltiplica(3, 4))
+
+
