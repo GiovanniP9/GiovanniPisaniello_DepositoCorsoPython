ee63eebf311a1b296ca60b30d0333722b14bd674
GiovanniP9 <pisaniellogiovanni53@gmail.com>
2025-04-16 17:57:28 +0200

    Create prova_numpy.py

---
+++ Mercoledi 16 Aprile/prova_numpy.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Creazione di un array unidimensionale
+arr = np.array([1, 2, 3, 4, 5])
+
+# Creazione di un array bidimensionale
+arr2d = np.array([[1, 2, 3], [4, 5, 6]])
+
+print("Array unidimensionale:", arr)
+print("Array bidimensionale:\n", arr2d)
