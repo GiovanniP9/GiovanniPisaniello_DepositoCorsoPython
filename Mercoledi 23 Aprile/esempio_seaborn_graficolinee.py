ac9578ee35a5329482a202f3d92efc292a379cfe
GiovanniP9 <pisaniellogiovanni53@gmail.com>
2025-04-23 16:37:01 +0200

    Update esempio_seaborn_graficolinee.py

--- Mercoledi 23 Aprile/esempio_seaborn_graficolinee.py
+++ Mercoledi 23 Aprile/esempio_seaborn_graficolinee.py
@@ -4,6 +4,6 @@ import matplotlib.pyplot as plt
 # Dati di esempio
 fmri = sns.load_dataset("fmri")
 # Creare un grafico a linee
-sns.lineplot(x="timepoint", y="signal", data=fmri, hue="region", style="event")
+sns.lineplot(x="timepoint", y="signal", data=fmri, hue="region", style="event") #hue="region" per colorare le linee in base alla regione e style="event" per differenziare gli eventi
 plt.title('Segnale FMRI nel Tempo')
 plt.show()
