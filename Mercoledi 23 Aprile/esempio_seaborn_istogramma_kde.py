ee5b46d6551a68e5ed366e5f7583334e00ab5625
GiovanniP9 <pisaniellogiovanni53@gmail.com>
2025-04-23 16:40:15 +0200

    Update esempio_seaborn_istogramma_kde.py

--- Mercoledi 23 Aprile/esempio_seaborn_istogramma_kde.py
+++ Mercoledi 23 Aprile/esempio_seaborn_istogramma_kde.py
@@ -1,9 +1,7 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
-#
 # Generare dati casuali
 data = sns.load_dataset("penguins")
-#
 # Creare un istogramma con KDE
 sns.histplot(data=data, x="flipper_length_mm", kde=True) # kde=True per aggiungere la densità
 plt.title('Distribuzione Lunghezza Pinne dei Pinguini')
